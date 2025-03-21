e79cf264f47521301b550f8ffa5f1802808f54a4
Malted <malted@malted.dev>
2025-03-21 14:52:50 -0400

    Assume SSL

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require "sidekiq/web"
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
-  mount ActionCable.server => "/cable"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
