e8270811f6a974ce9303f76f29bc0386d4d4ae57
Malted <malted@malted.dev>
2025-03-28 11:16:43 -0400

    Consume the schedules and match up users

--- app/jobs/check_schedules_job.rb
+++ app/jobs/check_schedules_job.rb
@@ -2,6 +2,14 @@ class CheckSchedulesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    users = UserSchedule.where("start < now() and finish > now()").map(&:user).uniq
+    if users.length % 2 == 1 # Not robust. Assumes max room size is 2. If it gets changed to higher than that it is more complex because 2 users can be in 3 person max room.
+      users.pop()
+    end
+
+    for user in users
+      user.holler
+    end
+
   end
 end
